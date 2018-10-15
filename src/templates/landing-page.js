3752bdade559ca12245a13e831c0613485d1cc28
Yaniv Keinan <yanivkeinan@gmail.com>
2018-10-15 21:08:42 +0300

    feat: add landing page

---
+++ src/templates/landing-page.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+import LandingPageView from "../views/LandingPage/LandingPage";
+
+const LandingPage = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <Layout>
+      <LandingPageView
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content={post.html}
+      />
+    </Layout>
+  );
+};
+
+LandingPage.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export default LandingPage;
+
+export const landingPageQuery = graphql`
+  query LandingPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
