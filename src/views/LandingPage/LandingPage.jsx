ba711faded2b91d6a12d6f3c4b4136a0b423e107
Yaniv Keinan <yanivkeinan@gmail.com>
2018-10-15 23:31:51 +0300

    test: change text for split testing

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -51,7 +51,7 @@ class LandingPage extends React.Component {
           <div className={classes.container}>
             <GridContainer>
               <GridItem xs={12} sm={6} md={6}>
-                <h1 className={classes.title}>Your Story Starts With Us.</h1>
+                <h1 className={classes.title}>Our Story Starts With You.</h1>
                 <h4>
                   Every landing page needs a small description after the big
                   bold title, that's why we added this text here. Add here all
