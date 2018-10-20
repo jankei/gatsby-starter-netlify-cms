1490da929933c09020e2fb089218d3b9a82ad793
Yaniv Keinan <yanivkeinan@gmail.com>
2018-10-21 02:19:27 +0300

    fix: rendering issue

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { createGenerateClassName, JssProvider } from "react-jss";
 
 import "./all.sass";
+
+const generateClassName = createGenerateClassName();
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Boxify" />
-    <div>{children}</div>
+    <JssProvider generateClassName={generateClassName}>
+      <div>{children}</div>
+    </JssProvider>
   </div>
 );
 
